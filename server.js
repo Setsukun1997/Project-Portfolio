26b000b996dbd965fe4de8643812e5414a0650da
Setsukun1997 <tety12308@hotmail.com>
2025-09-10 00:09:58 +0700

    Update server.js

--- server.js
+++ server.js
@@ -17,4 +17,5 @@ app.post('/transactions', async (req, res) => {
   res.json(newTx);
 });
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.listen(3000, () => console.log('Server running on http://localhost:3000'));
+
